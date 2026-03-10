582fedb1478be2aaece7eabf0cd8612aea1c74ee
Michael <134744264+FordEx18@users.noreply.github.com>
2026-03-10 19:50:28 +0000

    Created a Record for PaymentReceipt

---
+++ src/main/java/edu/norcocollege/cis18b/week3/PaymentReceipt.java
@@ -0,0 +1,5 @@
+//imports
+import java.math.BigDecimal;
+import java.time.Instant;
+//Record for PaymentReceipt
+public record PaymentReceipt(String orderId,BigDecimal amount,String method,Instant timestamp){}
