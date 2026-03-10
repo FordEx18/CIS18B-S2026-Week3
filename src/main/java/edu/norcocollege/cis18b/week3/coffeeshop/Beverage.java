537c68ae1e17217c580a670bed269a79a5e2d6d3
Michael <134744264+FordEx18@users.noreply.github.com>
2026-03-10 03:40:20 +0000

    Created Beverage Subclass & Methods

---
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/Beverage.java
@@ -0,0 +1,34 @@
+//imports
+import java.math.BigDecimal;
+//Class for Beverage, which extends MenuItem
+public class Beverage extends MenuItem{
+    //Enum for size of beverage
+    enum Size{
+        SMALL,
+        MEDIUM,
+        LARGE
+    }
+    //Private Variables
+    private Size size;
+    //Constructors
+    public Beverage(String sku,String name,BigDecimal price,Size size){
+        super(sku,name,price);
+        if(size==null){
+            throw new IllegalArgumentException("Size cannot be null!");
+        }
+        this.size=size;
+    }
+    //Size multiplier method
+    BigDecimal sizeMultiplier(){
+        switch(size){
+            case SMALL:
+                return BigDecimal.ONE;
+            case MEDIUM:
+                return new BigDecimal("1.2");
+            case LARGE:
+                return new BigDecimal("1.4");
+            default:
+                throw new IllegalStateException("Unexpected size: "+size);
+        }
+    }
+}
